753031e8960c15664be71f427efc0bf14b3faf17
Goran Kurel <38796068+rtgkurel@users.noreply.github.com>
2018-10-24 14:16:26 +0200

    Create ngrok-semaphore-setup.sh
    Taken from https://gist.githubusercontent.com/ervinb/618c3c805c516983c00ea052366348f3/raw/ngrok-semaphore-setup.sh.

---
+++ ngrok-semaphore-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ngrok_archive="ngrok-stable-linux-amd64.zip"
+ngrok_archive_path="${SEMAPHORE_CACHE_DIR}/${ngrok_archive}"
+ngrok_dl_url="https://bin.equinox.io/c/4VmDzA7iaHb/${ngrok_archive}"
+ngrok_local_api_url="localhost:4040/api"
+
+function wait_for_ngrok() {
+  echo -n "Waiting for ngrok to come online "
+
+  while true; do
+    printf "."
+
+    curl --fail ${ngrok_local_api_url}/tunnels &> /dev/null && break
+
+    sleep 1
+  done
+
+  printf "\n"
+}
+
+function setup() {
+  wget -O $ngrok_archive_path --no-clobber $ngrok_dl_url
+
+  echo "Y" | unzip $ngrok_archive_path -d $SEMAPHORE_PROJECT_DIR
+}
+
+function start_http() {
+  ./ngrok http 80 > /dev/null &
+}
+
+function get_public_url() {
+  echo -e "\nThe public URL is:"
+  curl -s ${ngrok_local_api_url}/tunnels | grep -oP '(?<="public_url":)[^,]*' | tail -n1
+}
+
+setup
+
+start_http
+
+wait_for_ngrok
+
+get_public_url
