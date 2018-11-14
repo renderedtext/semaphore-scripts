6a77f73be4f92722736f8761e3e24c650ddd8348
Nandor Bors <40260639+bogyo210@users.noreply.github.com>
2018-11-14 11:12:51 +0200

    Create s2_install_erlang20.3.sh
    Script should be run on semaphore2: bash s2_install_erlang20.3.sh
    Downloads a prebuilt  erlang 20.3, unpacks  and puts in the right place
    After running the script the new version should be activated by : . /home/semaphore/.kerl/installs/20.3/activate

---
+++ s2_install_erlang20.3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -H 'Host: packages.rt.com' --silent http://138.201.33.90/erlang/20.3.tar.gz --output 20.3.tar.gz
+tar -zxf 20.3.tar.gz
+rm -rf 20.3.tar.gz
+mv 20.3 ~/.kerl/installs/
+echo "20.3,20.3" >> ~/.kerl/otp_builds 
+echo "20.3 /home/semaphore/.kerl/installs/20.3" >> ~/.kerl/otp_installations 
