3ae1f45212d462f823f18427fd1c2d256f9885cd
Nandor Bors <40260639+bogyo210@users.noreply.github.com>
2018-11-14 14:41:12 +0200

    Update elixir's erlang source

--- s2_install_erlang20.3.sh
+++ s2_install_erlang20.3.sh
@@ -5,3 +5,5 @@ rm -rf 20.3.tar.gz
 mv 20.3 ~/.kerl/installs/
 echo "20.3,20.3" >> ~/.kerl/otp_builds 
 echo "20.3 /home/semaphore/.kerl/installs/20.3" >> ~/.kerl/otp_installations 
+#activate version 20 in elixir env
+sed -i 's/21/20/g' ~/.kiex/elixirs/elixir-1.7.3.env 
