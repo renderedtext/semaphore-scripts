bab7e26f66286a3dd098d05e361cc7f20e4fc170
Goran Kurel <38796068+rtgkurel@users.noreply.github.com>
2019-01-17 11:06:00 +0100

    Update java11install.sh
    Changed script a bit, to be easier to update the download link in the future. Changed the Description comments to the default format for `semaphore-scripts` repository.

--- java11install.sh
+++ java11install.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
-# Description : install oracle java 11 on sempahore platform
-# Runs: on semaphore 1 platform
-# Usage: wget -q -O - https://gist.githubusercontent.com/bogyo210/4cbd6951e5835da4aad5ca68e1a84f21/raw | bash
+
+####
+# Description: Installs Oracle Java JDK 11. Also uses caching so the future installations
+# are quicker.
+#
+# Runs on: All Semaphore Classic platforms
+#
+# Usage:
+# Add the following command to the setup of a build in the Project Settings
+#
+#    wget https://raw.githubusercontent.com/renderedtext/semaphore-scripts/master/java11install.sh && bash java11install.sh
+#
+# Note: Will overwrite any previous Oracle Java JDK 11 installation in `/usr/lib/jvm/java-11-oracle/`
+####
+
+set -e
+
+jdk_download_url=https://download.oracle.com/otn-pub/java/jdk/11.0.2+7/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.tar.gz
+cached_archive=$SEMAPHORE_CACHE_DIR/jdk-11_linux-x64_bin.tar.gz
+jdk11_path=/usr/lib/jvm/java-11-oracle/
 
 #downloading from Oracle
-if [ ! -f $SEMAPHORE_CACHE_DIR/jdk-11.0.1_linux-x64_bin.tar.gz ]; then
-    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/11.0.1+13/90cf5d8f270a4347a95050320eef3fb7/jdk-11.0.1_linux-x64_bin.tar.gz -O $SEMAPHORE_CACHE_DIR/jdk-11.0.1_linux-x64_bin.tar.gz
+if [ ! -f $cached_archive ]; then
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jdk_download_url -O $cached_archive
 fi
-tar -zxvf $SEMAPHORE_CACHE_DIR/jdk-*
+tar -zxvf $cached_archive
 #move into place
-sudo mv jdk-11.0.1 /usr/lib/jvm/java-11-oracle
+if [ -d $jdk11_path ]; then
+    rm -rf $jdk11_path
+fi
+sudo mkdir -p $jdk11_path
+sudo mv jdk-11*/* $jdk11_path
+rm -rf jdk-11*
 #install
 sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-11-oracle/bin/java  1111
 #select default
