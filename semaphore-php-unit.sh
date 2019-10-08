0886582adc86e02b308aed5671851b5f01134e89
csidyel <46521788+csidyel@users.noreply.github.com>
2019-10-08 10:43:38 +0200

    Update semaphore-php-unit.sh

--- semaphore-php-unit.sh
+++ semaphore-php-unit.sh
@@ -21,12 +21,12 @@
 set -e
 
 phpunit_ver=${1:-'6.5'}
-phpunit_cache_archive="$SEMAPHORE_CACHE_DIR/phpunit$phpunit_ver-cache"
+phpunit_cache_archive="$SEMAPHORE_CACHE_DIR/phpunit$phpunit_ver.phar"
 
 if [ -e $phpunit_cache_archive ]; then
   echo "Restoring phpunit installation cache..."
-  chmod +x $SEMAPHORE_CACHE_DIR/phpunit-$phpunit_ver
-  sudo mv $SEMAPHORE_CACHE_DIR/phpunit-$phpunit_ver /usr/local/bin/phpunit
+  chmod +x $SEMAPHORE_CACHE_DIR/phpunit-$phpunit_ver.phar
+  sudo mv $SEMAPHORE_CACHE_DIR/phpunit-$phpunit_ver.phar /usr/local/bin/phpunit
 else
   echo "Cached phpunit installation not found, downloading..."
   wget https://phar.phpunit.de/phpunit-$phpunit_ver.phar
